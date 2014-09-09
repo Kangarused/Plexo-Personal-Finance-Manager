107ad34e058ac36d7b1ef7ff3cf1828f5abb7b22
Adam Eury <adameury@outlook.com>
2014-09-09 15:51:44 -0400

    Enabled OAuth and setup Web API to allow CORS. Implemented Account Controller, a Repository class for authenticating users, and successfully tested API for user registration and token generation.

---
+++ PersonalFinance/PersonalFinance.API/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNet.Identity;
+using PersonalFinance.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PersonalFinance.API.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(model);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors to send, so send a Bad Request
+                    return BadRequest();
+                }
+
+                // Return the errors
+                return BadRequest(ModelState);
+            }
+            
+            return null;
+        }
+    }
+}
--- PersonalFinance/PersonalFinance.API/Startup.cs
+++ PersonalFinance/PersonalFinance.API/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using PersonalFinance.API.Providers;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -19,9 +22,26 @@ namespace PersonalFinance.API
             app.CreatePerOwinContext<SqlConnection>(() => new SqlConnection(connectionString));
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
+            ConfigureOAuth(app);
+
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config); // Register the Routes
+            app.UseCors(CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+        void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
