3bc680a772a73176ae7d82cf5e4427caa596aea5
Robert Jackson <robert.jackson@hotmail.com.au>
2016-08-24 13:05:07 +0930

    Imported Font Awesome files directly into package to remove the download delay seen when pulling resources from a CDN.

--- PersonalFinance/PersonalFinance.Common/Content/scripts/custom-angular/show-when-loading.js
+++ PersonalFinance/PersonalFinance.Common/Content/scripts/custom-angular/show-when-loading.js
@@ -1,30 +1,29 @@
 ﻿// a directive that shows or hides an element based on number of pending http requests
 angular.module('show.when.loading', [])
 
-    .directive('showWhenLoading', ['$http', function ($http) {
-        return {
-            restrict: 'E',
-            template: '<span><i class="fa fa-cog"></i></span>',//'<img src="Content/images/loading-animation.svg" height="{{height}}" alt="loading animation - data is loading" />',
-            link: function (scope, elm, attrs) {
+.directive('showWhenLoading', ['$http', function ($http) {
+    return {
+        restrict: 'E',
+        template: '<span><i class="fa fa-spin fa-cog"></i></span>',
+        link: function (scope, elm, attrs) {
 
-                if (attrs.small != null) {
-                    scope.height = "17";
-                } else {
-                    scope.height = "30"; 
-                }
-
-                scope.isLoading = function () {
-                    return $http.pendingRequests.length > 0;
-                };
-
-                scope.$watch(scope.isLoading, function (v) {
-                    if (v) {
-                        elm.show();
-                    } else {
-                        elm.hide();
-                    }
-                });
+            if (attrs.small != null) {
+                scope.height = "17";
+            } else {
+                scope.height = "30"; 
             }
-        };
 
-    }]);
+            scope.isLoading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+
+            scope.$watch(scope.isLoading, function (v) {
+                if (v) {
+                    elm.show();
+                } else {
+                    elm.hide();
+                }
+            });
+        }
+    };
+}]);
